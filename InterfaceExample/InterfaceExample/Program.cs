c808920311ed9dcaada88d7056c85e54711d93cd
Ayyanar <48320634+ayyanar-codes@users.noreply.github.com>
2023-07-25 11:46:57 +0530

    Add files via upload
    An interface defines a contract. Any class or struct that implements that contract must provide an implementation of the members defined in the interface. An interface may define a default implementation for members. It may also define static members in order to provide a single implementation for common functionality

---
+++ InterfaceExample/InterfaceExample/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AllMethods allMethods = new AllMethods();
+            allMethods.FirstMethod();
+            allMethods.SecondMethod();
+            allMethods.ThirdMethod();
+            allMethods.FourthMethod();
+            Console.ReadKey();
+        }
+
+        public interface iMethod1
+        {
+            void FirstMethod();
+            int SecondMethod();
+        }
+
+        public interface iMethod2
+        {
+            void ThirdMethod();
+            int FourthMethod();
+        }
+
+        public class AllMethods : iMethod1, iMethod2
+        {
+            public void FirstMethod()
+            {
+                Console.WriteLine("FirstMethod");
+            }
+
+            public int FourthMethod()
+            {
+                Console.WriteLine("FourthMethod");
+                return 12;
+            }
+
+            public int SecondMethod()
+            {
+                Console.WriteLine("SecondMethod");
+                return 12;
+            }
+
+            public void ThirdMethod()
+            {
+                Console.WriteLine("ThirdMethod");
+            }
+        }
+
+    }
+}
