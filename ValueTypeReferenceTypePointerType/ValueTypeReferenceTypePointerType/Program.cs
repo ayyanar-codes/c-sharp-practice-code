8249123115252c9c4511fea0e564d3995e0d10b5
Ayyanar <48320634+ayyanar-codes@users.noreply.github.com>
2023-07-25 11:48:25 +0530

    Add files via upload
    Reference Type. Unlike value types, a reference type doesn't store its value directly. Instead, it stores the address where the value is being stored.

---
+++ ValueTypeReferenceTypePointerType/ValueTypeReferenceTypePointerType/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValueTypeReferenceTypePointerType
+{
+    class Program
+    {
+
+        static void ChangeReferenceType(Student std2)
+        {
+            std2.StudentName = "Steve";
+        }
+
+
+        static void Main(string[] args)
+        {
+            Student std1 = new Student();
+            std1.StudentName = "Bill";
+
+            Student std2 = std1;
+
+            std1.StudentName = "Ayyanar";
+
+            //ChangeReferenceType(std2);
+
+            Console.WriteLine(std2.StudentName);
+            Console.ReadKey();
+        }
+    }
+
+    
+
+    public class Student
+    {
+        public string StudentName { get; set; }
+    }
+}
