1f679bba4933ef0f05fe8cfa1573522b7f204238
Ayyanar <48320634+ayyanar-codes@users.noreply.github.com>
2023-07-28 10:58:18 +0530

    Add files via upload

---
+++ c-sharp-namespace/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_namespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+
+    /*
+         Namespaces play an important role in managing related classes in C#. The .NET Framework uses namespaces to organize its built-in classes. For example, there are some built-in namespaces in 
+        .NET such as System, System.Linq, System.Web, etc. Each namespace contains related classes.
+
+        A namespace is a container for classes and namespaces. The namespace also gives unique names to its classes thereby you can have the same class name in different namespaces.
+        In C#, a namespace can be defined using the namespace keyword.
+     */
+
+    namespace School
+    {
+        // define classes here
+
+    }
+}
