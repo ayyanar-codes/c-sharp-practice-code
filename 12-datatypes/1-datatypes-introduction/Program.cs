13820d8012499cf65c822fb641c600e10405151d
Ayyanar <48320634+ayyanar-codes@users.noreply.github.com>
2023-08-08 09:22:17 +0530

    Add files via upload

---
+++ 12-datatypes/1-datatypes-introduction/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_datatypes_introduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringVar = "Hello World!!";
+            int intVar = 100;
+            float floatVar = 10.2f;
+            char charVar = 'A';
+            bool boolVar = true;
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 12-datatypes/2-byte/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_byte
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte b1 = 255;
+            //byte b2 = -128;// compile-time error: Constant value '-128' cannot be converted to a 'byte'
+            sbyte sb1 = -128;
+            sbyte sb2 = 127;
+            Console.ReadKey();
+        }
+    }
+}
