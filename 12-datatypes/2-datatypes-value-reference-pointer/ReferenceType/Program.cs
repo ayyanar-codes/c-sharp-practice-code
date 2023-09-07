d30ea3f154ab91cd70376ce0f3e8410c47857445
Ayyanar <48320634+ayyanar-codes@users.noreply.github.com>
2023-09-07 08:27:57 +0530

    Add files via upload

---
+++ 12-datatypes/2-datatypes-value-reference-pointer/ReferenceType/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReferenceType
+{
+    class Program
+    {
+
+        
+
+        static void Main(string[] args)
+        {
+            Student std1 = new Student();
+            string sName = "Ayyanar";
+            std1.StudentName = "Bill";
+            std1.Id = 2;
+            ChangeReferenceType(std1, sName);
+
+            Console.WriteLine(std1.StudentName);
+            Console.WriteLine(std1.Id);
+            Console.WriteLine(sName);
+            Console.ReadKey();
+        }
+
+        static void ChangeReferenceType(Student std2, string name)
+        {
+            std2.StudentName = "Steve";
+            std2.Id = 22;
+            name = "Ayyanar.A";
+        }
+
+        public class Student
+        {
+            public string StudentName { get; set; }
+            public int Id { get; set; }
+        }
+    }
+}
---
+++ 12-datatypes/2-datatypes-value-reference-pointer/ValueType/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValueType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 100;
+
+            Console.WriteLine(i);
+
+            ChangeValue(i);
+
+            Console.WriteLine(i);
+
+            Console.ReadKey();
+        }
+
+
+        static void ChangeValue(int x)
+        {
+            x = 200;
+
+            Console.WriteLine(x);
+        }
+    }
+}
